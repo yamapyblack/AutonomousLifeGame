7e3521414b79a8b2f4326f93427d71f563e4385a
yamapyblack <s25050730r@gmail.com>
2023-05-23 12:10:26 +0900

    for vercel

--- packages/client/src/mud/getNetworkConfig.ts
+++ packages/client/src/mud/getNetworkConfig.ts
@@ -15,13 +15,13 @@ type NetworkConfig = SetupContractConfig & {
 export async function getNetworkConfig(): Promise<NetworkConfig> {
   const params = new URLSearchParams(window.location.search);
 
-  // const chainId = Number(import.meta.env.CHAIN_ID);
-  // const worldAddress = import.meta.env.WORLD_ADDRESS;
-  // const initialBlockNumber = import.meta.env.INITIAL_BLOCK_NUMBER;
+  const chainId = Number(import.meta.env.CHAIN_ID);
+  const worldAddress = import.meta.env.WORLD_ADDRESS;
+  const initialBlockNumber = import.meta.env.INITIAL_BLOCK_NUMBER;
 
-  const chainId = Number(420);
-  const worldAddress = "0x12DF9BEA3f7Cc25e385A30a0F53C317Bdeaa5E42";
-  const initialBlockNumber = 9690067;
+  // const chainId = Number(420);
+  // const worldAddress = "0x12DF9BEA3f7Cc25e385A30a0F53C317Bdeaa5E42";
+  // const initialBlockNumber = 9690067;
 
   // const chainId = Number(
   //   params.get("chainId") || import.meta.env.VITE_CHAIN_ID || 31337
@@ -35,11 +35,13 @@ export async function getNetworkConfig(): Promise<NetworkConfig> {
   // const world = worlds[chain.id.toString()];
   // const worldAddress = params.get("worldAddress") || world?.address;
   // if (!worldAddress) {
-  //   throw new Error(`No world address found for chain ${chainId}. Did you run \`mud deploy\`?`);
+  //   throw new Error(
+  //     `No world address found for chain ${chainId}. Did you run \`mud deploy\`?`
+  //   );
   // }
   // const initialBlockNumber = params.has("initialBlockNumber")
-  // ? Number(params.get("initialBlockNumber"))
-  // : world?.blockNumber ?? -1; // -1 will attempt to find the block number from RPC
+  //   ? Number(params.get("initialBlockNumber"))
+  //   : world?.blockNumber ?? -1; // -1 will attempt to find the block number from RPC
 
   return {
     clock: {
@@ -51,11 +53,11 @@ export async function getNetworkConfig(): Promise<NetworkConfig> {
       chainId,
       // jsonRpcUrl: params.get("rpc") ?? chain.rpcUrls.default.http[0],
       // wsRpcUrl: params.get("wsRpc") ?? chain.rpcUrls.default.webSocket?.[0],
-      // jsonRpcUrl: import.meta.env.JSON_RPC_URL,
-      // wsRpcUrl: import.meta.env.WS_RPC_URL,
-      jsonRpcUrl: "https://goerli.optimism.io",
-      wsRpcUrl:
-        "wss://opt-goerli.g.alchemy.com/v2/aWFq-OVN3tnqGVRIStXbwf1ApF3xqWEg",
+      jsonRpcUrl: import.meta.env.JSON_RPC_URL,
+      wsRpcUrl: import.meta.env.WS_RPC_URL,
+      // jsonRpcUrl: "https://goerli.optimism.io",
+      // wsRpcUrl:
+      //   "wss://opt-goerli.g.alchemy.com/v2/aWFq-OVN3tnqGVRIStXbwf1ApF3xqWEg",
     },
     // privateKey: getBurnerWallet().value,
     privateKey: import.meta.env.PRIVATE_KEY,
